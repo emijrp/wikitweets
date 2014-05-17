499ed0c516b08d807e633e8a01df182d27625b9b
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-05-17 13:33:56 +0200

    adding login.py script to generate tokens from keys

---
+++ login.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014 emijrp
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from twython import Twython
+
+def read_keys():
+    f = open('.twitter_keys', 'r')
+    w = f.read()
+    f.close()
+    APP_KEY = re.findall(ur'(?im)^APP_KEY\s*=\s*([^\n]+?)\s*$', w)[0].strip()
+    APP_SECRET = re.findall(ur'(?im)^APP_SECRET\s*=\s*([^\n]+?)\s*$', w)[0].strip()
+    return APP_KEY, APP_SECRET
+
+def write_tokens():
+    OAUTH_TOKEN = final_step['oauth_token']
+    OAUTH_TOKEN_SECRET = final_step['oauth_token_secret']
+    
+    f = open('.twitter_tokens', 'w')
+    f.write('OAUTH_TOKEN = %s\nOAUTH_TOKEN_SECRET = %s' % (OAUTH_TOKEN, OAUTH_TOKEN_SECRET))
+    f.close()
+
+def main():
+    APP_KEY, APP_SECRET = read_keys()
+    
+    twitter = Twython(APP_KEY, APP_SECRET)
+    auth = twitter.get_authentication_tokens()
+    OAUTH_TOKEN = auth['oauth_token']
+    OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+
+    print auth['auth_url']
+
+    pin = raw_input("Open the URL and insert code here: ")
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    final_step = twitter.get_authorized_tokens(pin)
+    
+    write_tokens(final_step)
+
+if __name__ == '__main__':
+    main()
